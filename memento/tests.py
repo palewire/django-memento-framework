4f4d246193bcfb66ddeaf74e6854b658796fdb0f
palewire <ben.welsh@gmail.com>
2015-05-09 18:44:01 -0700

    Added setup and tests

---
+++ memento/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+
+class MementoTest(TestCase):
+
+    def test_memento(self):
+        pass
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from distutils.core import Command
+
+
+class TestCommand(Command):
+    user_options = []
+    package = 'memento'
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from django.conf import settings
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'NAME': ':memory:',
+                    'ENGINE': 'django.db.backends.sqlite3'
+                }
+            },
+            INSTALLED_APPS=(self.package,),
+            MIDDLEWARE_CLASSES=()
+        )
+        from django.core.management import call_command
+        import django
+        if django.VERSION[:2] >= (1, 7):
+            django.setup()
+        call_command('test', self.package)
+
+
+setup(
+    name='django-memento',
+    version='0.0.1',
+    description='A set for helpers for Django web sites to enable the \
+Memento framework for time-based access',
+    author='Ben Welsh',
+    author_email='ben.welsh@gmail.com',
+    url='http://www.github.com/datadesk/django-memento/',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,  # because we're including static files
+    install_requires=(
+        'python-dateutil',
+    ),
+    cmdclass={'test': TestCommand}
+)
